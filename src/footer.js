f0269550e5fe19e8e75048001fb0cec20c9c6dc2
bhasker <bhaskarp2347@gmail.com>
2022-08-16 00:02:02 +0530

    asdasd

--- src/footer.js
+++ src/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     Wish you happy birthday ...i know paper hai 😂 but koi ni all the best paper k liye 
     paper deke maje krio party hard ..stay safe and  take care of yourself ..party pending h saari 
     ek sath hisab hoga sb 😛
-    me esa khi bhej ni skta tha to esa hi bna k bhej dia 😅...
+    me esa khi bhej ni skta tha to esa hi bna k bhej dia 😅... happpy birthday once again
   </div>;
 };
 

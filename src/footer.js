3c32e698d8cbe41d77d269b3d1ba06697cfcdf4a
bhasker <bhaskarp2347@gmail.com>
2022-08-15 23:55:55 +0530

    sdasda

--- src/footer.js
+++ src/footer.js
@@ -1,7 +1,11 @@
 import react from "react";
 import "./styles.css";
 const Footer = () => {
-  return <div className="footer-container">fghgfad gsfhgsh</div>;
+  return <div className="footer-container">
+    Wish you happy birthday ...i know paper hai 😂 but koi ni all the best paper k liye 
+    paper deke maje krio party hard ..stay safe and  take care of yourself ..
+    me esa khi bhej ni skta tha to esa hi bna k bhej dia 😅...
+  </div>;
 };
 
 export default Footer;

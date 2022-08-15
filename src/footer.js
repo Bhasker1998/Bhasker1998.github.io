55fab11952e7b0c222b0856329109248800b6e77
bhasker <bhaskarp2347@gmail.com>
2022-08-15 23:59:00 +0530

    asdfasdf

--- src/footer.js
+++ src/footer.js
@@ -3,7 +3,8 @@ import "./styles.css";
 const Footer = () => {
   return <div className="footer-container">
     Wish you happy birthday ...i know paper hai 😂 but koi ni all the best paper k liye 
-    paper deke maje krio party hard ..stay safe and  take care of yourself ..
+    paper deke maje krio party hard ..stay safe and  take care of yourself ..party pending h saari 
+    ek sath hisab hoga sb 😛
     me esa khi bhej ni skta tha to esa hi bna k bhej dia 😅...
   </div>;
 };
